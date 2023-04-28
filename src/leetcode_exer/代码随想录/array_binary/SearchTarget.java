3e04c75d0869e1eb411723fae1af7eb58b90a3e5
Zhiyi233 <73516600+Zhiyi233@users.noreply.github.com>
2023-04-28 17:13:19 +0800

    recrusive way

--- src/leetcode_exer/代码随想录/array_binary/SearchTarget.java
+++ src/leetcode_exer/代码随想录/array_binary/SearchTarget.java
@@ -28,20 +28,37 @@ public class SearchTarget {
      */
 
     public int search(int[] nums,int target){
-        int left=0;
-        int right = nums.length-1;
+        int size = nums.length;
+        int left = 0;
+        int right = size-1;
 
+        // while(left<=right){
+        // int middle = left+(right-left)/2;
+        //     if(nums[middle] > target){
+        //         right = middle-1;
+        //     }else if(nums[middle]<target){
+        //         left = middle+1;
+        //     }else{
+        //         return middle;
+        //     }
+        // }
+        // return -1;
+
+        return binarySearch(nums,target,left,right);
+
+    }
+    public int binarySearch(int[] nums,int target,int left,int right){
+        //递归方法找
         while(left<=right){
-            int mid=left+(right-left)/2;
-            if(nums[mid]<target){
-                left=mid+1;
-            }else if(nums[mid]>target){
-                right = mid-1;
-            }else {
+            int mid = left+(right-left)/2;
+            if(nums[mid]==target){
                 return mid;
+            }else if(nums[mid]<target){
+                return binarySearch(nums,target,mid+1,right);
+            }else{
+                return binarySearch(nums,target,left,mid-1);
             }
         }
-        //此时说明数组中找不到对应的target
         return -1;
     }
 
